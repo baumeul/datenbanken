b6de1d7ac8552c4ca4b30d6412af42c7798e52d9
Ulrich Baumeister <ulrich.baumeister@me.com>
2017-09-09 20:19:32 +0200

    pg_connect

--- psycopg2/pg_connect.py
+++ psycopg2/pg_connect.py
@@ -2,6 +2,6 @@ import psycopg2
 
 con = psycopg2.connect(database='immo', user='postgres', password='4score7', host='localhost')
 cursor = con.cursor()
-cursor.execute("select * from im_banken")
-for row in cursor:
-    print(row)
+cursor.execute("select count(*) from im_umsaetze")
+for c in cursor:
+    print(str(c))
